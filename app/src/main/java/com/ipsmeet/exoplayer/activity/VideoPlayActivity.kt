e4cf52ed86c377d04b19f2d640bd0337b7bb1c7d
IPS-030 <meetp-itpath@gmail.com>
2023-07-04 16:21:00 +0530

    DONE: -[X] video vise screen orientation

--- app/src/main/java/com/ipsmeet/exoplayer/activity/VideoPlayActivity.kt
+++ app/src/main/java/com/ipsmeet/exoplayer/activity/VideoPlayActivity.kt
@@ -1,18 +1,22 @@
 package com.ipsmeet.exoplayer.activity
 
+import android.content.pm.ActivityInfo
+import android.media.MediaFormat
 import android.os.Bundle
+import android.os.Handler
 import android.view.Window
 import android.view.WindowManager
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.media3.common.C
+import androidx.media3.common.Format
 import androidx.media3.common.MediaItem
 import androidx.media3.common.util.UnstableApi
 import androidx.media3.datasource.DefaultDataSourceFactory
 import androidx.media3.exoplayer.ExoPlayer
-import androidx.media3.exoplayer.source.ConcatenatingMediaSource
-import androidx.media3.exoplayer.source.ProgressiveMediaSource
+import androidx.media3.exoplayer.source.*
+import androidx.media3.exoplayer.video.VideoFrameMetadataListener
 import com.bumptech.glide.Glide
 import com.google.common.net.HttpHeaders.USER_AGENT
 import com.ipsmeet.exoplayer.R
@@ -50,11 +54,44 @@ class VideoPlayActivity : AppCompatActivity() {
         val mediaItem = MediaItem.fromUri(videoPlayList[position].path!!)
         val dataSourceFactory = DefaultDataSourceFactory(this, USER_AGENT)
         val concatenatingMediaSource = ConcatenatingMediaSource()
+
+        var currentOrientation = ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
+
+        // listen for video size changes
+        val videoListener = object : VideoFrameMetadataListener {
+            override fun onVideoFrameAboutToBeRendered(presentationTimeUs: Long, releaseTimeNs: Long, format: Format, mediaFormat: MediaFormat?) {
+                val newOrientation = if (format.width > format.height) {
+                    ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+                } else {
+                    ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+                }
+
+                // update the activity's orientation if it has changed
+                if (newOrientation != currentOrientation) {
+                    currentOrientation = newOrientation
+                    requestedOrientation = currentOrientation
+                }
+            }
+        }
+
         for (i in 0..videoPlayList.size) {
             val mediaSourceFactory = ProgressiveMediaSource.Factory(dataSourceFactory)
                 .createMediaSource(mediaItem)
             concatenatingMediaSource.addMediaSource(mediaSourceFactory)
         }
+
+        concatenatingMediaSource.addEventListener(Handler(), object : MediaSourceEventListener {
+            override fun onLoadCompleted(windowIndex: Int, mediaPeriodId: MediaSource.MediaPeriodId?, loadEventInfo: LoadEventInfo, mediaLoadData: MediaLoadData) {
+                super.onLoadCompleted(windowIndex, mediaPeriodId, loadEventInfo, mediaLoadData)
+                if (mediaLoadData.trackFormat != null) {
+                    requestedOrientation = if (mediaLoadData.trackFormat!!.width > mediaLoadData.trackFormat!!.height) {
+                        ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+                    } else {
+                        ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+                    }
+                }
+            }
+        })
 
         exoPlayer = ExoPlayer.Builder(this).build()
         binding.playerView.apply {
@@ -67,8 +104,13 @@ class VideoPlayActivity : AppCompatActivity() {
             play()
             playWhenReady = true
             seekTo(position, C.TIME_UNSET)
+            setVideoFrameMetadataListener(videoListener)
         }
 
+        videoController()
+    }
+
+    private fun videoController() {
         //  PLAY-PAUSE
         findViewById<ImageView>(R.id.imgV_playPause).setOnClickListener {
             if (exoPlayer.playWhenReady) {
@@ -86,16 +128,28 @@ class VideoPlayActivity : AppCompatActivity() {
 
         //  NEXT
         findViewById<ImageView>(R.id.imgV_next).setOnClickListener {
-            exoPlayer.stop()
-            position++
-            playVideo()
+            try {
+                exoPlayer.stop()
+                position++
+                findViewById<TextView>(R.id.txt_videoTitle).text = videoPlayList[position].displayName
+                playVideo()
+            }
+            catch (e: Exception) {
+                finish()
+            }
         }
 
         //  PREVIOUS
         findViewById<ImageView>(R.id.imgV_previous).setOnClickListener {
-            exoPlayer.stop()
-            position--
-            playVideo()
+            try {
+                exoPlayer.stop()
+                position--
+                findViewById<TextView>(R.id.txt_videoTitle).text = videoPlayList[position].displayName
+                playVideo()
+            }
+            catch (e: Exception) {
+                finish()
+            }
         }
 
         //  FORWARD
@@ -116,20 +170,7 @@ class VideoPlayActivity : AppCompatActivity() {
             pause()
             playbackState
         }
-        /*Glide.with(this).load(R.drawable.round_play_arrow_24)
-            .into(findViewById(R.id.imgV_playPause))*/
     }
-
-    /*override fun onStop() {
-        super.onStop()
-        exoPlayer.apply {
-            playWhenReady = false
-            stop()
-            playbackState
-        }
-        Glide.with(this).load(R.drawable.round_play_arrow_24)
-            .into(findViewById(R.id.imgV_playPause))
-    }*/
 
     override fun onRestart() {
         super.onRestart()
